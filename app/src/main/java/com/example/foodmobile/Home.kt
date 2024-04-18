bbbe0ae0033fd567d64c4f820654568c1828e7a9
NathanChapelle <ncdev@outlook.fr>
2024-04-18 15:06:37 +0200

    theme work fine

--- app/src/main/java/com/example/foodmobile/Home.kt
+++ app/src/main/java/com/example/foodmobile/Home.kt
@@ -1,15 +1,23 @@
 package com.example.foodmobile
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodmobile.ui.theme.FoodMobileTheme
 
-@Preview(name = "Home Screen", showBackground = true)
+//En attendant l'implémentation api
+val recipes = arrayOf(
+    {
+
+    }
+)
 @Composable
 fun Home(modifier: Modifier = Modifier){
     Column(
@@ -19,6 +27,14 @@ fun Home(modifier: Modifier = Modifier){
     )
     {
         Title(title = "Home")
-        Text(text = "test")
+        Text(text = "test", color = MaterialTheme.colorScheme.primary)
+    }
+}
+
+@Preview(name = "Home", showBackground = true)
+@Composable
+fun HomePreview(){
+    FoodMobileTheme {
+        Home()
     }
 }
