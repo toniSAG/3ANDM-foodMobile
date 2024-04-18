ecba7756da29a64e75a172711ef3f04ad2400566
NathanChapelle <ncdev@outlook.fr>
2024-04-18 15:21:30 +0200

    created a recipe class

---
+++ app/src/main/java/com/example/foodmobile/Recipe.java
@@ -0,0 +1,12 @@
+package com.example.foodmobile;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Recipe {
+    public Integer pk;
+    public String title;
+    public String image;
+    public String updatedAt;
+    public ArrayList<String> ingredients;
+}
