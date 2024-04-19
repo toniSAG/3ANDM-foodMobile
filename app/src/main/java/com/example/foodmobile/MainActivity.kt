e795579a02d1b15780aab180650e6221823bcfd4
NathanChapelle <ncdev@outlook.fr>
2024-04-19 17:40:26 +0200

    added a splash screen to the app

--- app/src/main/java/com/example/foodmobile/MainActivity.kt
+++ app/src/main/java/com/example/foodmobile/MainActivity.kt
@@ -3,21 +3,25 @@ package com.example.foodmobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.foodmobile.ui.theme.FoodMobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             FoodMobileTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
                     FoodMobileTheme {
                         val loading = false
